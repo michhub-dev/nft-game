50491f460d9af36c84ba5d6c23f24d242b6e57eb
Michelle David <rachaelholy@gmail.com>
2022-06-02 16:04:43 +0100

    First commit

---
+++ contracts/nft-game.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+import "hardhat/console.sol";
+
+contract NftGame {
+    constructor() {
+        console.log("Hey, this is my NFT game!");
+    }
+}
