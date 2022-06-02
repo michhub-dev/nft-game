0402556d488f6505206e3a55d6b28c324f969c33
Michelle David <rachaelholy@gmail.com>
2022-06-02 17:59:23 +0100

    Updated with comments and fix bug

--- contracts/nft-game.sol
+++ contracts/nft-game.sol
@@ -1,6 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 
 pragma solidity ^0.8.0;
+
+/// @title Turn based NFT game
+/// @author Michelle
+/// @notice Building an NFT game 
+
 import "hardhat/console.sol";
 
 contract NftGame {
--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,9 @@
 const main = async () => {
+    ///@dev this will compile the contract and generate neccesary file to work with the contract under artifect directory
     const gameContractFactory = await hre.ethers.getContractFactory("NftGame");
     const contractFactory = await gameContractFactory.deploy();
+
+    ///@dev wait for the contract to officially mine and deploy to the local blockchain 
     await contractFactory.deployed();
     console.log("Contract deployed to..", contractFactory.address);
 }
