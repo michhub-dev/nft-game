880cf5f2f660d741343e24309baa22a388c33d9f
Michelle David <rachaelholy@gmail.com>
2022-06-04 14:44:29 +0100

    Imported & inherited from Openzeppelin

--- contracts/nft-game.sol
+++ contracts/nft-game.sol
@@ -6,9 +6,19 @@ pragma solidity ^0.8.0;
 // @author Michelle
 // @notice Building an NFT game 
 
+//NFT contract to inherit from 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+// Helper functions from OpenZeppelin.
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+
 import "hardhat/console.sol";
 
-contract NftGame {
+// Inherit from openzeppelin, the standard NFT contract 
+ contract NftGame is ERC721 {
+
 // @notice the character's attribute stored here
 struct PersonaAttributes {
  string name;
@@ -31,6 +41,7 @@ PersonaAttributes[] defaultPersonaAttributes;
         uint256[] memory personaDefense,
         uint256[] memory personaEnergyLevel
     )
+    ERC721 ("Hero Game", "HOGE")
     {
     /*  @notice loop through all the attributes and save the values in the contract,
      to be used later when an NFT is minted */
