34455d8270bd707be0324b8f0aa11f88d6976a38
Michelle David <rachaelholy@gmail.com>
2022-06-11 04:40:08 +0100

    Call attack func, log

--- scripts/run.js
+++ scripts/run.js
@@ -16,18 +16,29 @@ const main = async () => {
         50000, //Boss Hp
         40 // Boss attack damage 
     );
+   
 
     //@notice wait for the contract to officially mine and deploy to the local blockchain 
     await contractFactory.deployed();
     console.log("Contract deployed to..", contractFactory.address);
 
+    console.log("------------------------------------------------------------");
     // call the mint function here
     let txn;
     txn = await contractFactory.mintNft(3);
     await txn.wait();
 
+    console.log("------------------------------------------------------------");
+  // Call attack function
     txn = await contractFactory.attackBossBaby();
     await txn.wait();
+
+    console.log("------------------------------------------------------------");
+
+    txn - await contractFactory.attackBossBaby();
+    await txn.wait(); 
+
+    console.log("------------------------------------------------------------");
 
     /* Get the value of the NFT URI 
     tokenURI is a function on every NFT that returns the actual data attached to the NFT
