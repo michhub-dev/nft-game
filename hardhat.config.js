6c5724eb60552eca03c97dc36d2a9431b88f19a1
Michelle David <rachaelholy@gmail.com>
2022-06-14 15:57:02 +0100

    Updated address and ABI

--- hardhat.config.js
+++ hardhat.config.js
@@ -8,7 +8,7 @@ module.exports = {
       url: process.env.STAGING_ALCHEMY_KEY,
       accounts: [process.env.PRIVATE_KEY],
     },
-   /* mainnet: {
+    /*mainnet: {
       chainId: 1,
       url: process.env.PROD_ALCHEMY_KEY,
       accounts: [process.env.PRIVATE_KEY],
