6aed90c0ce70bdc2e801238ba66252a66fa5c46c
Michelle David <rachaelholy@gmail.com>
2022-06-14 23:50:29 +0100

    Defined method for persona Data

--- nft-game-reactjs/src/constants.js
+++ nft-game-reactjs/src/constants.js
@@ -1,3 +1,12 @@
 const CONTRACT_ADDRESS = "0xa8E3f4C25989213e353183bd2907C440F44a097B";
 
-export {CONTRACT_ADDRESS};
+const transformPersonaData = (personaData) => {
+    return {
+      name: personaData.name,
+      imageUrl: personaData.imageUrl,
+      Hp: personaData.Hp.toNumber(),
+      maxHp: personaData.maxHp.toNumber(),
+      attackDamage: personaData.attackDamage.toNumber(),
+}
+};
+export {CONTRACT_ADDRESS, transformPersonaData};
