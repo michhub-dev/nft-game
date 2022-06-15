47978ca28de258f57b58a00d1793c1b4d5516219
Michelle David <rachaelholy@gmail.com>
2022-06-15 03:58:28 +0100

    Imported files, updated component

--- nft-game-reactjs/src/Components/SelectPersona/index.js
+++ nft-game-reactjs/src/Components/SelectPersona/index.js
@@ -1,7 +1,32 @@
+import { ethers } from 'ethers';
 import React, { useEffect, useState } from 'react';
+import { CONTRACT_ADDRESS } from '../../constants';
 import './SelectPersona.css';
 
 const SelectPersona = ({ setPersonaNFT }) => {
+    
+    const [attributes, setAttributes] = useState([]);
+    const [gameContract, setGameContract] = useState(null);
+
+    useEffect(() =>{
+     
+        const {ethereum} = window;
+
+        if(ethereum) {
+
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const signer = provider.getSigner();
+            const gameContract = new ethers.Contract(CONTRACT_ADDRESS, NftGame.abi, signer);
+      // store in state variable
+      setGameContract(gameContract);
+
+        } else {
+            console.log("Ethereum object not found");
+        }
+       
+
+    },[])
+
     return (
         <div className='select-character-container'>
         <h2>Mint your hero to play the game</h2>
--- nft-game-reactjs/src/index.js
+++ nft-game-reactjs/src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import React, { useEffect, useState } from 'react';
+import SelectPersona from './Components/SelectPersona';
+import { CONTRACT_ADDRESS, transformPersonaData } from './constants';
+import { ethers } from 'ethers';
+import NftGame from './utils/NftGame.json';
 
 ReactDOM.render(
   <React.StrictMode>
