31feea01abd10ac5bdfd40a0641318cc3dd85f2c
Michelle David <rachaelholy@gmail.com>
2022-06-17 16:56:38 +0100

    Action to call mint function. Changed image URI

--- nft-game-reactjs/src/Components/SelectPersona/index.js
+++ nft-game-reactjs/src/Components/SelectPersona/index.js
@@ -4,77 +4,90 @@ import { ethers } from 'ethers';
 import { CONTRACT_ADDRESS, transformPersonaData } from '../../constants';
 import NftGame from '../../utils/NftGame.json';
 
+
 const SelectPersona = ({ setPersonaNFT }) => {
-    
-    const [personas, setPersonas] = useState([]);
-    const [gameContract, setGameContract] = useState(null);
+   const [ personas, setPersonas ] = useState([]);
+   const [ gameContract, setGameContract ] = useState(null);
+  
+   // Action to call mint function 
+   const mintPersonaNFTAction = async (personaId) => {
+     try{
 
-    useEffect(() =>{
-        const {ethereum} = window;
-        if(ethereum) {
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
-            const signer = provider.getSigner();
-            const gameContract = new ethers.Contract(CONTRACT_ADDRESS, NftGame.abi, signer);
-            // store in state variable
-            setGameContract(gameContract);
-
-        } else {
-            console.log("Ethereum object not found");
+        if (gameContract) {
+            console.log("Minting persona...");
+            const mintTXN = await gameContract.mintNft(personaId);
+            await mintTXN.wait();
+            console.log("mintTXN", mintTXN);
         }
-    },[]);
+     } catch (error) {
+        console.warn("mintPersonaNFTAction", error);
+     }
+   } 
 
-    useEffect(() => {
-        const getPersonas = async() => {
-            try {
-                console.log("Getting contract personas to mint")
-             
-                // Call contract to get all mintable personas
-                const PersonaTXN = await gameContract.retrieveDefaultAttributes();
-                console.log("PersonaTXN", PersonaTXN);
-             
-                // Go through all the data and transform them
-                const personas = PersonaTXN.map((personaData) => {
-                    transformPersonaData(personaData);
-                });
-               
-                // Set all the mintable characters in state
-                setPersonas(personas);
-            } catch (error) {
-                console.log( "Something went wrong while fetching data", error);
-            }
+  useEffect(() =>{
+    const {ethereum} = window;
+
+    if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const gameContract = new ethers.Contract(CONTRACT_ADDRESS, NftGame.abi, signer);
+       
+        // Set the gameContract in state
+        setGameContract(gameContract)
+    } else {
+        console.log("Ethereum object not found")
+    }
+
+  },[]);
+
+  useEffect(() => {
+    const getPersonas = async() => {
+        try{
+            console.log("Trying to get contract to mint...");
+
+            //call contract to get all mintable personas
+            const personasTXN = await gameContract.retrieveDefaultAttributes();
+            console.log("personasTXN", personasTXN);
+
+            // Go through all the personas and transform the data
+            const personas = personasTXN.map((personaData) => {
+                transformPersonaData(personaData);
+            });
+            // Set all mintable personas in state
+            setPersonas(personas);
+        } catch(error) {
+          console.log("Something went wrong fetching personas. Couldn't get personas", error);
         }
+    };
+    // If gameContract is ready, get personas
+    if (gameContract) {
+        getPersonas();
+    }
+  },[gameContract]);
+  
+  //Render function
+  const renderPersonas = () => 
+     personas.map((persona, index) => (
+        <div className='character-item' key={persona.name}>
+            <div className='name-container'>
+                <p>{persona.name}</p>
+            </div>
+            <img src={persona.imageURI} alt={persona.name} 
+                className="character-mint-button" />
+            <button onClick={() => mintPersonaNFTAction(index)}>
+                {`Mint ${persona.name}`}
+             </button>
+        </div>
+     ));
 
-        // If the gameContract is ready, then display personas 
-        if(gameContract) {
-            getPersonas(); 
-        }
-    },[gameContract]);
-
-       // Render method
-
-       const renderPersonas = () => 
-       personas.map((persona, index) => (
-           <div className="character-item" key={persona.name}>
-               <div className="name-container" >
-                   <p>{persona.name}</p>
-               </div>
-               <img src={persona.imageURI} alt={persona.name} />
-               <button type="button" className="character-mint-button" 
-               //onClick={() => mintPersonaNftAction(index)}
-               >{`Mint ${persona.name}`}</button>
-           </div>
-       ));
-
-    return(
-        <div className="select-character-container">
-            <h2>Mint your hero to play the game</h2>
-            {/**only show this when there are personas in state */}
-            {personas.length > 0 && (
-                <div className="character-grid">{renderPersonas()}</div>
-            )}
-       </div>
-    );
-
-};
-
-export default SelectPersona;
+   return (
+    <div className='select-character-container'>
+        <h2>Mint your hero to play the game. Choose wisely!</h2>
+     {/* Only show this when there are personas in state */}
+     {personas.length > 0 && (
+        <div className='character-grid'>{renderPersonas()}</div>
+     )}
+    </div>
+   );
+}
+ export default SelectPersona;
